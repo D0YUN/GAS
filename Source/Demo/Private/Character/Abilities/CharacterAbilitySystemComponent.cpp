8979f4906df432035e8ad6b3d9e7643b9432e276
D0YUN <191944956+D0YUN@users.noreply.github.com>
2025-07-04 18:47:40 +0900

    [feat] GAS 유튜브 - 01~03 초입

---
+++ Source/Demo/Private/Character/Abilities/CharacterAbilitySystemComponent.cpp
@@ -0,0 +1,6 @@
+#include "Character/Abilities/CharacterAbilitySystemComponent.h"
+
+void UCharacterAbilitySystemComponent::ReceiveDamage(UCharacterAbilitySystemComponent* SoureASC, float UnmitigatedDamage, float MitigatedDamage)
+{
+    ReceivedDamage.Broadcast(SoureASC, )
+}
---
+++ Source/Demo/Public/Character/Abilities/CharacterAbilitySystemComponent.h
@@ -0,0 +1,24 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AbilitySystemComponent.h"
+#include "CharacterAbilitySystemComponent.generated.h"
+
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_ThreeParams(FReceivedDamageDelegate, UCharacterAbilitySystemComponent*, SourceASC, float, UnmitigatedDamage, float, MitigatedDamage)
+
+UCLASS()
+class DEMO_API UCharacterAbilitySystemComponent : public UAbilitySystemComponent
+{
+	GENERATED_BODY()
+	
+public:
+	bool CharacterAbilitiesGivn = false;
+	bool StartupEffectsApplied = false;
+
+	FReceivedDamageDelegate ReceivedDamage;
+
+	// 데미지를 받은 곳, 
+	virtual void ReceiveDamage(UCharacterAbilitySystemComponent* SoureASC, float UnmitigatedDamage, float MitigatedDamage);
+};
